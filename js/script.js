4ba60b89c58a33064a962e1c46cac5c2880599f2
Jasmine <70559611+Jasmine582@users.noreply.github.com>
2021-10-27 20:28:41 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const tripInfoButton = document.querySelector(".trip-info");
+const dailyBudget = document.querySelector(".daily-budget");
+
+const tripInfo = function () {
+  const totalBudget = Number(prompt("What is your total budget?"));
+  const accomodation = Number(prompt("What are your accomodation costs?"));
+  const numDays = Number(prompt("How many days does your trip last?"));
+
+  calculateDailyBudget(totalBudget, accomodation, numDays);
+};
+
+const calculateDailyBudget = function (totalBudget, accommodation, numDays) {
+  const daily = ((totalBudget - accommodation) / numDays).toFixed(2);
+  dailyBudget.innerText = `You can spend $${daily} a day on food and fun.`;
+};
+
+tripInfoButton.addEventListener("click", tripInfo);
